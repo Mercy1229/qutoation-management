44d02ca51eae3f58eed6730f8eae4bcfdc261101
Mercy1229 <136076453+Mercy1229@users.noreply.github.com>
2026-06-21 22:16:15 +0530

    Add files via upload

--- src/api/pdfService.ts
+++ src/api/pdfService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE = "https://quotation-management-be.onrender.com";
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+const API_BASE = "https://quotation-management-be.onrender.com";
+// const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
 
 export interface EventService {
   service: string;
